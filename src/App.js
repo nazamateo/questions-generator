aaea9f4ba90d7379c35902865e49eb70e2e75952
nazamateo <nazamateo@gmail.com>
2022-12-07 22:22:09 +0800

    Add semantic UI and page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Form, TextArea, Button } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="app-header">
+        <h2 className="header">Quiz Master</h2>
+      </div>
+
+      <div className="app-body">
+        <div>
+          <Form>
+            <Form.Field
+              control={TextArea}
+              placeholder="Type lesson to generate questions from.."
+            />
+
+            <select className="questions-count-select">
+              <option>Please select how many questions you want.</option>
+            </select>
+
+            <Form.Field control={TextArea} placeholder="Your Questions..." />
+
+            <Button color="orange" size="large">
+              <i class="paper plane outline icon"></i>
+              Generate
+            </Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
